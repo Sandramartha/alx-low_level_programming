eb93f88caa3dbf2e9013c8c2f2198cb1ebff5fa3
Sandramartha <omotolamartha2002@gmail.com>
2023-05-21 17:14:09 -0700

    task 0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+# include <unisted.h>
+int main (void)
+{
+	write 2,
+	      "and that part of art us useful\" - Dora Kopar, 2015-10-19\n",59);
+	return 1;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,3 @@
+printf("size of a char is %lu",sizeof(char));
+
+continue with this pattern for everything on the list.
