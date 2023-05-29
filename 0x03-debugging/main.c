679fe78623d21033b6b3f7d65c8951019e01b689
Sandramartha <omotolamartha2002@gmail.com>
2023-05-29 16:42:27 -0700

    task o

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* positive_or_negative - checks for positive or negative numbers
+*
+* #i: the number to be checked
+*
+* return: always zero
+* 
+* void positive_or_negative (int i)
+*/
+
+int main(void)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+}
