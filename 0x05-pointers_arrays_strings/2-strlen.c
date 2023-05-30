ea25c87b24b666eff75e06b90e9446aff78e221f
Sandramartha <omotolamartha2002@gmail.com>
2023-05-29 20:27:34 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,1 @@
-#include "main.h"
-/**
- * _strlen - returns the length of a string
- * @s: string
- * Return: lenght
- */
-int_strlen(char *s)
-{
-	int longi = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-
-	return (longi);
-}
